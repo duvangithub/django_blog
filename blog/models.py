941b1a2f09ecef34ed90bb7f111ef8db621fd11f
Duvan Zavaleta <duvzavaleta@gmail.com>
2025-01-13 22:44:52 -0500

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    #Permite cambiar el nombre de la categoria a plural
     class Meta:
         verbose_name_plural = "categories"
 
