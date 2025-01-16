f0dbfd5d5be49857f0a9342154f74a0e2125f138
Duvan <duvzavaleta@gmail.com>
2025-01-16 18:11:01 -0500

    Current changes

--- blog/models.py
+++ blog/models.py
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-from django.db import models
-
-# Create your models here.
-
-# blog/models.py
-
-class Category(models.Model):
-    name = models.CharField(max_length=30)
-
-    class Meta:
-        verbose_name_plural = "categories"
-
-    #Permite ver el nombre de la clase en el backend del admin
-    def __str__(self):
-        return self.name
-
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
-    categories = models.ManyToManyField("Category", related_name="posts")
-
-    #Permite ver el nombre de la clase en el backend del admin
-    def __str__(self):
-        return self.title
-
-class Comment(models.Model):
-    author = models.CharField(max_length=60)
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey("Post", on_delete=models.CASCADE)
-
-    #Permite ver el nombre de la clase en el backend del admin
-    def __str__(self):
-        return f"{self.author} on '{self.post}'"
-=======
 from django.db import models
 
 # Create your models here.
@@ -74,4 +36,3 @@ class Comment(models.Model):
     #Permite ver el nombre de la clase en el backend del admin
     def __str__(self):
         return f"{self.author} on '{self.post}'"
->>>>>>> 59e61d335a8682aa485311c599a1c739b9736b32
--- blog/tests.py
+++ blog/tests.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from django.test import TestCase
 
 # Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 59e61d335a8682aa485311c599a1c739b9736b32
